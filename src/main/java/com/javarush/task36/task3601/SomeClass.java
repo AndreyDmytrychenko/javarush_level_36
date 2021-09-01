a7386a6ba6d38f279ae912fab077ae518c1544d1
Andrey <dmitrich27@ukr.net>
2021-09-02 02:01:58 +0300

    change line 6

---
+++ src/main/java/com/javarush/task36/task3601/SomeClass.java
@@ -0,0 +1,4 @@
+package com.javarush.task36.task3601;
+
+public class SomeClass {
+}
--- src/test/Solution.java
+++ src/test/Solution.java
@@ -3,5 +3,6 @@ package test;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("Line");
     }
 }
